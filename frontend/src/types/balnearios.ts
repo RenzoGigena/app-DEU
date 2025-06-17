d74433319f09b76a7f4105936dcd4b83d7a31882
RenzoGigena <renzogigena1@gmail.com>
2025-06-17 11:15:12 -0300

    arreglos de web aria y confirmacion de aprobacion

--- frontend/src/types/balnearios.ts
+++ frontend/src/types/balnearios.ts
@@ -13,3 +13,14 @@ export type Balneario = {
 	detalle: string
 	servicios: Servicio[]
 }
+
+export type Solicitud = {
+	id: string
+	nombreBalneario: string
+	localidad: string
+	descripcion: string
+	servicios: string[]
+	telefono: string
+	url: string
+	contribuidor: string
+}
