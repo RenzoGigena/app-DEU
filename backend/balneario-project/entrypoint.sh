782282eb0a2efe24eea3581bd12faf53a209c99e
RenzoGigena <renzogigena1@gmail.com>
2025-07-28 10:48:10 -0300

    dockerizado completo con seed db

---
+++ backend/balneario-project/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "📦 Esperando a que Postgres esté disponible..."
+
+until nc -z postgres 5432; do
+  sleep 2
+done
+
+echo "🚀 Aplicando migraciones..."
+npx prisma migrate deploy
+
+if [ ! -f /app/.seeded ]; then
+  echo "🌱 Ejecutando seed..."
+  npm run seed && touch /app/.seeded
+else
+  echo "✅ Seed ya ejecutado anteriormente."
+fi
+
+echo "🏁 Iniciando servidor..."
+npm run start:dev
